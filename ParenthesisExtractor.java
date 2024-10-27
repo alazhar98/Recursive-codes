51a3b17034ae196b15df8520a943c729eaedff62
alazhar <alazhar9814@gmail.com>
2024-10-27 10:27:37 +0400

    check if is empty return nothing

---
+++ ParenthesisExtractor.java
@@ -0,0 +1,10 @@
+public class ParenthesisExtractor {
+    public static void main(String[] args){
+
+    }
+    public static String parenBit(String str){
+        if(str.isEmpty()){
+            return "";
+        }
+    }
+}
