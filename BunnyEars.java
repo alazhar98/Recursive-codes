5c0e7035a4912c8d8f922b5cf9c36944b656c62b
alazhar <alazhar9814@gmail.com>
2024-10-27 08:49:00 +0400

    Recursive Bunny Ears Counting

--- BunnyEars.java
+++ BunnyEars.java
@@ -22,6 +22,7 @@ public class BunnyEars {
     public static int bunnyEars2(int n){
         if(n==0){
             return 0;
+
         }
         if (n % 2 == 1) {
             return 2 + bunnyEars2(n - 1);
