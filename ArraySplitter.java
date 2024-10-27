9c45deeecf7c2c7816632409b7bd4fcc075d9747
alazhar <alazhar9814@gmail.com>
2024-10-27 11:15:34 +0400

    calculate the sum of nums in array

---
+++ ArraySplitter.java
@@ -0,0 +1,13 @@
+public class ArraySplitter {
+    public static void main(String[] args ){
+
+    }
+    public static boolean split53(int[] nums){
+        int totalSum = 0;
+        // calculate the sum of nums in array
+        for (int num : nums){
+            totalSum+=num;
+
+        }
+    }
+}
