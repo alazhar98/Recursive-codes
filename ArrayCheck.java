0ddd247fed13c177223393233f30ff4b1b86cd6c
alazhar <alazhar9814@gmail.com>
2024-10-27 10:04:11 +0400

    make main function

--- ArrayCheck.java
+++ ArrayCheck.java
@@ -1,7 +1,9 @@
 public class ArrayCheck {
     public static void main(String[] args){
 
-
+        System.out.println(array220(new int[]{1, 2, 20}, 0));
+        System.out.println(array220(new int[]{3, 30}, 0));
+        System.out.println(array220(new int[]{3}, 0));
 
 
     }
