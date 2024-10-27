99e4bd631ee6ad917cccfa885dac4f8cf5de8b23
alazhar <alazhar9814@gmail.com>
2024-10-27 11:33:51 +0400

    return statement

--- ArraySplitter.java
+++ ArraySplitter.java
@@ -1,23 +1,25 @@
 public class ArraySplitter {
-    public static void main(String[] args ){
+    public static void main(String[] args) {
 
     }
-    public static boolean split53(int[] nums){
+
+    public static boolean split53(int[] nums) {
         int totalSum = 0;
         // calculate the sum of nums in array
-        for (int num : nums){
-            totalSum+=num;
+        for (int num : nums) {
+            totalSum += num;
         }
         // check if total sum is odd we can not split it evenly
-        if (totalSum%2!=0){
+        if (totalSum % 2 != 0) {
             return false;
         }
-        return canBeSplit(nums,0,totalSum/2,0,0);
+        return canBeSplit(nums, 0, totalSum / 2, 0, 0);
     }
+
     private static boolean canBeSplit(int[] nums, int index, int targetSum, int group5Sum, int group3Sum) {
         // if reach the end of the array
-        if (index==nums.length){
-            return group5Sum==targetSum && group3Sum==targetSum;
+        if (index == nums.length) {
+            return group5Sum == targetSum && group3Sum == targetSum;
         }
         int num = nums[index];
 
@@ -25,9 +27,13 @@ public class ArraySplitter {
         if (num % 5 == 0) {
             return canBeSplit(nums, index + 1, targetSum, group5Sum + num, group3Sum);
         }
-        // If the number is a multiple of 3 but not 5 add it to the group3Sum
+        // If the number is a multiple of 3 but not 5 add to the group3Sum
         else if (num % 3 == 0) {
             return canBeSplit(nums, index + 1, targetSum, group5Sum, group3Sum + num);
-
+        }
+        // else try to place the number in group3Sum or group5Sum
+        return canBeSplit(nums, index + 1, targetSum, group5Sum + num, group3Sum) ||
+                canBeSplit(nums, index + 1, targetSum, group5Sum, group3Sum + num);
     }
 }
+
