4f3472f1ce80f8a862eeda281a315a669c446bae
alazhar <alazhar9814@gmail.com>
2024-10-27 10:46:34 +0400

    check if str does not contain sub

---
+++ SubstringDistance.java
@@ -0,0 +1,9 @@
+public class SubstringDistance {
+    public static void main(String[] args){
+
+    }
+    public static int strDist(String str, String sub){
+        if (!str.contains(sub)) {
+            return 0;
+    }
+}
