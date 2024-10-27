c6457e304d17856da53db3939838e0721d8229bd
alazhar <alazhar9814@gmail.com>
2024-10-27 10:39:24 +0400

    Recursive Extraction of Parenthesis and Contents

--- ParenthesisExtractor.java
+++ ParenthesisExtractor.java
@@ -1,27 +1,26 @@
 public class ParenthesisExtractor {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(parenBit("xyz(abc)123"));
         System.out.println(parenBit("x(hello)"));
         System.out.println(parenBit("(xy)1"));
     }
-    public static String parenBit(String str){
+
+    public static String parenBit(String str) {
         // check if str is empty return nothing
-        if(str.isEmpty()){
+        if (str.isEmpty()) {
             return "";
         }
         //check if str is start by ( and leave the others before
-        if(str.charAt(0)=='('){
+        if (str.charAt(0) == '(') {
             // check If the last character is ) then return the substring
-            if(str.charAt(str.length()-1)==')'){
+            if (str.charAt(str.length() - 1) == ')') {
                 return str;
             }
             // if nit call again to the end
-            return parenBit(str.substring(0,str.length()-1));
-
-            }
-        // if start not equal to ( call again until it found
-        return parenBit(str.substring(1));
+            return parenBit(str.substring(0, str.length() - 1));
 
         }
+        // if start not equal to ( call again until it found
+        return parenBit(str.substring(1));
     }
 }
