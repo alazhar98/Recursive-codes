00b457f2241cb8ff919901351fe7b8dfccecee0b
alazhar <alazhar9814@gmail.com>
2024-10-27 10:02:56 +0400

    make array220 function

---
+++ ArrayCheck.java
@@ -0,0 +1,17 @@
+public class ArrayCheck {
+    public static void main(String[] args){
+
+
+
+
+    }
+    public static boolean array220(int[] nums, int index) {
+        if(index>=nums.length-1){
+            return false;
+        }
+        if (nums[index] *10==nums[index +1]){
+            return true;
+        }
+        return array220(nums,index+1);
+    }
+}
