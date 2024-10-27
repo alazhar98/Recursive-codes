f4e242c65e38a42d3444fbb9b1baf3b3a1d8be68
alazhar <alazhar9814@gmail.com>
2024-10-27 10:17:02 +0400

    add main function

--- StringSeparator.java
+++ StringSeparator.java
@@ -1,8 +1,16 @@
-
+import java.util.Scanner;
 
 public class StringSeparator {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string :");
 
+        String input = scanner.nextLine();
+
+        String result = allStar(input);
+        System.out.println("the result is :"+result);
+
+        scanner.close();
     }
 
     public static String allStar(String str) {
