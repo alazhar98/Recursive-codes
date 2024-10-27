d27162bcb3f9bc3df11094bedf9f3ff5ffc6e5aa
alazhar <alazhar9814@gmail.com>
2024-10-27 10:06:25 +0400

    make some comments

--- ArrayCheck.java
+++ ArrayCheck.java
@@ -7,6 +7,11 @@ public class ArrayCheck {
 
 
     }
+    /*
+    first check if if the array has fewer than 2 elements remaining from the current index return false
+    check if the element at the current index is followed by that element multiplied by 10 at the next index return true
+    If not found, move to the next index by calling
+     */
     public static boolean array220(int[] nums, int index) {
         if(index>=nums.length-1){
             return false;
