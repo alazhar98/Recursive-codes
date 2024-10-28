5eadbb1b12bf6e6ad104e4cb16c2500fabb35fcc
alazhar <alazhar9814@gmail.com>
2024-10-28 08:48:58 +0400

    if the number is multiple of 5 and not 1

--- SumGroupChecker.java
+++ SumGroupChecker.java
@@ -10,10 +10,14 @@ public class SumGroupChecker {
         int current = nums[start];
         // if current number is multiple if 5
         if (current%5==0){
-            // if next number is 1
+            // if next number is 1 skip the number
             if(start+1 <nums.length && nums[start+1]==1){
                 return groupSum5(start + 2, nums, target - current);
 
+            }
+            // if the number is multiple of 5 and not 1
+            else {
+                groupSum5(start+1,nums,target-current);
             }
         }
     }
