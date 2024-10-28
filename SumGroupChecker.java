892c69f4cf617fd9bfe0df14cbab2d1f040750a6
alazhar <alazhar9814@gmail.com>
2024-10-28 08:41:52 +0400

    if reach the end of the array

---
+++ SumGroupChecker.java
@@ -0,0 +1,11 @@
+public class SumGroupChecker {
+    public static void main(String[] args){
+
+    }
+    public static boolean groupSum5(int start, int[] nums, int target) {
+        if(start>= nums.length){
+            return target == 0;
+        }
+
+    }
+}
