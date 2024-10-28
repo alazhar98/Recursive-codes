007a4b8d9e1b9bbadc36982cfca70068f5885510
alazhar <alazhar9814@gmail.com>
2024-10-28 12:11:03 +0400

    if the target is 0 then valid group is founded

--- NonAdjacentSum.java
+++ NonAdjacentSum.java
@@ -3,6 +3,10 @@ public class NonAdjacentSum {
 
     }
     public static boolean groupNoAdj(int start, int[] nums, int target) {
+        // if the target is 0 then valid group is founded
+        if (target==0){
+            return true;
+        }
 
     }
 }
