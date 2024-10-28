0585db3684b9c2c15850e4b1b40bb7a600419378
alazhar <alazhar9814@gmail.com>
2024-10-28 10:17:47 +0400

    if reach the end of the array

---
+++ ClumpSumChecker.java
@@ -0,0 +1,11 @@
+public class ClumpSumChecker {
+    public static void main(String[] args ){
+
+    }
+    public static boolean groupSumClump(int start, int[] nums, int target) {
+        // if reach the end of the array
+        if (start>=nums.length){
+            return target==0;
+        }
+    }
+}
