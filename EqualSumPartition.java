9828d3e8e24d73ee7372a3fd6c6b0203c8643240
alazhar <alazhar9814@gmail.com>
2024-10-28 10:53:26 +0400

    make 3 function

---
+++ EqualSumPartition.java
@@ -0,0 +1,11 @@
+public class EqualSumPartition {
+    public static void main(String[] args){
+
+    }
+    public static boolean splitArray(int[] nums) {
+
+    }
+    private static boolean splitHelper(int[] nums, int index, int target) {
+
+    }
+}
