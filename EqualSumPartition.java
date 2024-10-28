3f416d05843f2ea8d67fcb5d57d53b6bb4f07453
alazhar <alazhar9814@gmail.com>
2024-10-28 10:56:22 +0400

    calculate the sum

--- EqualSumPartition.java
+++ EqualSumPartition.java
@@ -3,7 +3,12 @@ public class EqualSumPartition {
 
     }
     public static boolean splitArray(int[] nums) {
+        int sum =0;
 
+        // calculate the sum
+        for (int num : nums){
+            sum=sum+num;
+        }
     }
     private static boolean splitHelper(int[] nums, int index, int target) {
 
