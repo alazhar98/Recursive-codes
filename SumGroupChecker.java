9e081207fe53b99fa42fc5fe186bebeb03cb0e16
alazhar <alazhar9814@gmail.com>
2024-10-28 08:45:50 +0400

    if current number is multiple if 5

--- SumGroupChecker.java
+++ SumGroupChecker.java
@@ -3,9 +3,17 @@ public class SumGroupChecker {
 
     }
     public static boolean groupSum5(int start, int[] nums, int target) {
+        //if reach the end of the array
         if(start>= nums.length){
             return target == 0;
         }
+        int current = nums[start];
+        // if current number is multiple if 5
+        if (current%5==0){
+            if(start+1 <nums.length && nums[start+1]==1){
+                return groupSum5(start + 2, nums, target - current);
 
+            }
+        }
     }
 }
