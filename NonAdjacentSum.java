097d34253cb062b3fd99d7ecee5e70406cdda9c1
alazhar <alazhar9814@gmail.com>
2024-10-29 11:35:49 +0400

    commit

--- NonAdjacentSum.java
+++ NonAdjacentSum.java
@@ -5,7 +5,7 @@ public class NonAdjacentSum {
         int[] nums = {2, 5, 10, 4};
         System.out.println(canSelectGroup(0, nums, 12));
         System.out.println(canSelectGroup(0, nums, 14));
-        System.out.println(canSelectGroup(0,nums, 7));
+        System.out.println(canSelectGroup(0, nums, 7));
 
     }
     public static boolean canSelectGroup(int start, int[] nums, int target) {
@@ -13,21 +13,19 @@ public class NonAdjacentSum {
         if (target==0){
             return true;
         }
-        // if sum has exceed the target
-        if (target<0){
-            return false;
-        }
+        // if sum has exceeded the target
         // if reach the last element in the array
-        if (start>=nums.length){
+        if (target < 0 || start >= nums.length) {
             return false;
         }
+
         // include current number + skip the next number
-        boolean includeCurrent = canSelectGroup(start+2 , nums , target-nums[start]);
+        boolean withCurrent = canSelectGroup(start+2 , nums , target - nums[start]);
 
         // Skip current number
-        boolean skipCurrent = canSelectGroup(start+1,nums , target);
+        boolean withoutCurrent = canSelectGroup(start+1 , nums , target);
 
-        return includeCurrent|| skipCurrent;
+        return withCurrent || withoutCurrent;
 
    }
 }
