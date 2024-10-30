6f3f71a03a764bf270ff010c85c55cd5c488ef88
alazhar <alazhar9814@gmail.com>
2024-10-30 11:13:35 +0400

    format the code

--- TriangleBlocks.java
+++ TriangleBlocks.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class TriangleBlocks {
-    public static void main(String[] args ){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the number of rows in the triangle:");
         int n = scanner.nextInt();
 
-        int totalBlocks =triangle(n);
+        int totalBlocks = triangle(n);
         System.out.println("Total number of blocks: " + totalBlocks);
 
         scanner.close();
@@ -17,10 +17,10 @@ public class TriangleBlocks {
         others if second row then will be total number of blocks 2+(2-1)=3
          */
 
-    public static int triangle(int n){
-        if(n==0){
+    public static int triangle(int n) {
+        if (n == 0) {
             return 0;
         }
-        return n+ triangle(n-1);
+        return n + triangle(n - 1);
     }
 }
