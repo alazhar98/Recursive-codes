46d6c3647ca6f2f1e0b1e24b26454439ada04304
alazhar <alazhar9814@gmail.com>
2024-10-31 11:17:03 +0400

    add School function

--- SchoolSystem.java
+++ SchoolSystem.java
@@ -3,13 +3,13 @@ import java.util.*;
 
 class Subject {
     String name;
-    String teacher ;
+    String teacher;
     int creditHours;
     double marks;
     String grade;
     String comments;
 
-    public Subject(String name,String teacher,int creditHours,String grade,  double marks,  String comments) {
+    public Subject(String name, String teacher, int creditHours, String grade, double marks, String comments) {
         this.name = name;
         this.creditHours = creditHours;
         this.teacher = teacher;
@@ -17,6 +17,7 @@ class Subject {
         this.comments = comments;
         this.marks = marks;
     }
+
 }
 
 class Student{
@@ -53,6 +54,25 @@ public class SchoolSystem{
 
     public static void addSchool(String name , String address){
         schools.put(name, new School(name,address));
+    }
+    public static void addStudent(String schoolName, String studentId, String name, int grade, int age){
+        if (!schools.containsKey(schoolName)){
+            System.out.println("there is no School with this name ");
+            return;
+        }
+
+        Student student= new Student(studentId,name,grade,age);
+        schools.get(schoolName).students.put(studentId,student);
+
+
+    }
+    public static void addSubject(String schoolName , String studentId, String subjectName, String teacher, int creditHours, double marks, String grade, String comments){
+        if (!schools.containsKey(schoolName) || !schools.get(schoolName).students.containsKey(studentId)) {
+            System.out.println("There is no student or There is no School");
+            return;
+        }
+        Subject subject = new Subject(subjectName, teacher, creditHours, marks, grade, comments);
+        schools.get(schoolName).students.get(studentId).subjects.put(subjectName,subject);
     }
 
 
