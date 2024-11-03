1c53a9660417de7f1901f5f3efc4f2dd367e8428
alazhar <alazhar9814@gmail.com>
2024-11-03 11:32:13 +0400

    delete comment

--- SchoolSystem.java
+++ SchoolSystem.java
@@ -20,7 +20,7 @@ public class SchoolSystem {
             System.out.println("4. Exit");
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            scanner.nextLine();
 
             switch (choice) {
                 case 1:
