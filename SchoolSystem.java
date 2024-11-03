a7310bfc230d8783580c48366b5b8d227c73779c
alazhar <alazhar9814@gmail.com>
2024-11-03 09:12:35 +0400

    changes

--- SchoolSystem.java
+++ SchoolSystem.java
@@ -9,7 +9,7 @@ public class SchoolSystem {
         // HashMaps for schools and students
         Map<String, Map<String, Object>> schools = new HashMap<>();
 
-        // Adding schools with their addresses
+        // Adding schools with their addresses and unique students
         schools.put("School A", createSchool("School A", "123 muscat"));
         schools.put("School B", createSchool("School B", "456 alrustaq"));
         schools.put("School C", createSchool("School C", "789 fanja"));
@@ -45,11 +45,37 @@ public class SchoolSystem {
         schoolInfo.put("Address", address);
 
         List<Map<String, Object>> students = new ArrayList<>();
-        students.add(createStudent(1, "Alazhar", "10", 15, 85, 78, 90));
-        students.add(createStudent(2, "Fisal", "10", 16, 76, 82, 88));
-        students.add(createStudent(3, "Mohammed", "10", 15, 90, 77, 95));
-        students.add(createStudent(4, "Salim", "10", 16, 80, 85, 87));
-        students.add(createStudent(5, "Ahmed", "10", 15, 90, 77, 99));
+        if (schoolName.equals("School A")) {
+            students.add(createStudent(1, "Alice", "10", 15, 85, 78, 90));
+            students.add(createStudent(2, "Bob", "10", 16, 76, 82, 88));
+            students.add(createStudent(3, "Charlie", "10", 15, 90, 77, 95));
+            students.add(createStudent(4, "David", "10", 16, 80, 85, 87));
+            students.add(createStudent(5, "Eva", "10", 15, 90, 77, 99));
+        } else if (schoolName.equals("School B")) {
+            students.add(createStudent(1, "Frank", "10", 15, 88, 76, 85));
+            students.add(createStudent(2, "Grace", "10", 16, 70, 80, 90));
+            students.add(createStudent(3, "Hannah", "10", 15, 95, 70, 75));
+            students.add(createStudent(4, "Ian", "10", 16, 60, 85, 80));
+            students.add(createStudent(5, "Jack", "10", 15, 78, 80, 85));
+        } else if (schoolName.equals("School C")) {
+            students.add(createStudent(1, "Kate", "10", 15, 92, 81, 88));
+            students.add(createStudent(2, "Leo", "10", 16, 70, 90, 84));
+            students.add(createStudent(3, "Mia", "10", 15, 85, 75, 95));
+            students.add(createStudent(4, "Nina", "10", 16, 88, 80, 90));
+            students.add(createStudent(5, "Owen", "10", 15, 77, 82, 78));
+        } else if (schoolName.equals("School D")) {
+            students.add(createStudent(1, "Paul", "10", 15, 75, 90, 80));
+            students.add(createStudent(2, "Quinn", "10", 16, 88, 77, 83));
+            students.add(createStudent(3, "Rose", "10", 15, 80, 85, 90));
+            students.add(createStudent(4, "Sam", "10", 16, 90, 82, 84));
+            students.add(createStudent(5, "Tina", "10", 15, 70, 88, 79));
+        } else if (schoolName.equals("School E")) {
+            students.add(createStudent(1, "Uma", "10", 15, 91, 89, 95));
+            students.add(createStudent(2, "Victor", "10", 16, 84, 82, 90));
+            students.add(createStudent(3, "Wendy", "10", 15, 76, 88, 80));
+            students.add(createStudent(4, "Xander", "10", 16, 79, 85, 78));
+            students.add(createStudent(5, "Yara", "10", 15, 85, 90, 92));
+        }
 
         schoolInfo.put("Students", students);
         return schoolInfo;
